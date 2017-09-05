17b4bbc41e8512eed52adac795f3b34e05fc4148
Howard Deiner <howard.deiner@deinersoft.com>
2017-09-05 17:31:30 -0400

    Initial commit.

--- src/main/java/com/deinersoft/timeteller/Demo.java
+++ src/main/java/com/deinersoft/timeteller/Demo.java
@@ -9,9 +9,6 @@ import java.io.IOException;
 class Demo {
     public static void main(String [] args) throws MessagingException, IOException{
 
-        Email eMail = new Email("configEmail.properties");
-        Sms sms = new Sms("config.Sms.properties");
-
         System.out.println(new TimeFormatterNumeric(new Clock()).formatTime());
         System.out.println(new TimeFormatterNumeric(new Clock("UTC")).formatTime());
         System.out.println(new TimeFormatterNumeric(new Clock("Europe/Kiev")).formatTime());
@@ -19,6 +16,7 @@ class Demo {
         System.out.println(new TimeFormatterApproximateWording(new Clock("UTC")).formatTime());
         System.out.println(new TimeFormatterApproximateWording(new Clock("Europe/Kiev")).formatTime());
 
+        Email eMail = new Email("configEmail.properties");
         eMail.send(new TimeFormatterNumeric(new Clock()).formatTime());
         eMail.send(new TimeFormatterNumeric(new Clock("UTC")).formatTime());
         eMail.send(new TimeFormatterNumeric(new Clock("Europe/Kiev")).formatTime());
@@ -26,6 +24,7 @@ class Demo {
         eMail.send(new TimeFormatterApproximateWording(new Clock("UTC")).formatTime());
         eMail.send(new TimeFormatterApproximateWording(new Clock("Europe/Kiev")).formatTime());
 
+        Sms sms = new Sms("config.Sms.properties");
         sms.send(new TimeFormatterNumeric(new Clock()).formatTime());
         sms.send(new TimeFormatterNumeric(new Clock("UTC")).formatTime());
         sms.send(new TimeFormatterNumeric(new Clock("Europe/Kiev")).formatTime());
